9daedbadf43f1b1cd2d4e0d40ca660562c32285b
Yevhen Utyekhin <yutyekhin@zscaler.com>
2025-06-17 19:33:59 -0700

    - added release branch github workflow

---
+++ .github/workflows/scripts/build_common_libraries.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+prql_common_functions_path="/prql/common_functions"
+result_path="common_libraries"
+existed_dialects=$(find "$(pwd)$prql_common_functions_path" -mindepth 1 -maxdepth 1 -type d ! -name "generic" | sort | xargs -n1 basename)
+mkdir "./$result_path"
+
+for dialect in $existed_dialects; do
+  dialect_name=$(echo "$dialect" | cut -d'_' -f1)
+  files=$(find "$(pwd)$prql_common_functions_path" -type f -name '*.prql' | grep -E "generic|$dialect_name" | sort)
+
+  for file in $files; do
+    if [[ "$file" == *"generic"* ]]; then
+      echo "# ===== GENERIC $(basename "$file") =====" >> "$(pwd)/$result_path/$dialect_name.prql"
+    else
+      echo "# ===== $(echo $dialect_name | tr '[:lower:]' '[:upper:]') $(basename $file) =====" >> "$(pwd)/$result_path/$dialect_name.prql"
+    fi
+    cat "$file" >> "$(pwd)/$result_path/$dialect_name.prql"
+    echo >> "$(pwd)/$result_path/$dialect_name.prql"
+    echo >> "$(pwd)/$result_path/$dialect_name.prql"
+  done
+done
